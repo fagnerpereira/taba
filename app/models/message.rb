72ed26a39678b910ac47f895d91f122f793e30b1
Fagner Rosa <fagnerfpr@gmail.com>
2026-02-28 23:28:14 -0300

    add messages basic creation with some tests

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,14 +2,19 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :community
   belongs_to :parent_message, class_name: "Message", optional: true
-  has_many :replies, class_name: "Message", foreign_key: "parent_message_id", dependent: :destroy
+
+  has_many :replies,
+    class_name: "Message",
+    foreign_key: "parent_message_id",
+    dependent: :destroy
   has_many :reactions, dependent: :destroy
 
   attribute :username, :string
+  validates :username, :content, :user_ip, presence: true
 
   before_validation :set_user
 
   def set_user
-    self.user = user.presence || User.find_or_create_by(username: username)
+    self.user = User.find_or_create_by!(username: username) if username.present?
   end
 end
